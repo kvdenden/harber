db97a782b29de5475ddb00fc9a5030f2e25f9a5e
Ken Van den Enden <ken@enden.be>
2023-10-31 16:16:07 +0100

    Create initial version of Harber contract.

---
+++ src/Harber.sol
@@ -0,0 +1,259 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.21;
+
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {Address} from "@openzeppelin/contracts/utils/Address.sol";
+
+import {IHarber} from "./interfaces/IHarber.sol";
+
+abstract contract Harber is ERC721, IHarber {
+    using Address for address;
+
+    uint256 public immutable reservePrice;
+
+    uint256 public immutable gracePeriod = 24 hours;
+
+    uint256 public immutable feePeriod = 365 days;
+    uint256 public immutable feeRate = 5_00; // 5%
+    uint256 public immutable feeDenominator = 100_00;
+
+    address public beneficiary;
+
+    mapping(uint256 => uint256) _prices;
+    mapping(uint256 => uint256) _tokenBalances;
+    mapping(uint256 => uint256) _lastSettlements;
+
+    mapping(address => uint256) _accountBalances;
+
+    uint256 _nonce;
+    mapping(uint256 => Bid) _bids;
+
+    modifier onlyAuthorized(uint256 tokenId) {
+        _checkAuthorized(ownerOf(tokenId), _msgSender(), tokenId);
+        _;
+    }
+
+    modifier onlySolvent(uint256 tokenId) {
+        _checkSolvent(tokenId);
+        _;
+    }
+
+    modifier onlyOwnedByContract(uint256 tokenId) {
+        address owner = ownerOf(tokenId);
+        if (owner != address(this)) revert ERC721IncorrectOwner(address(this), tokenId, owner);
+        _;
+    }
+
+    modifier notOwnedByContract(uint256 tokenId) {
+        address owner = ownerOf(tokenId);
+        if (owner == address(this)) revert ERC721IncorrectOwner(address(this), tokenId, owner);
+        _;
+    }
+
+    function buy(uint256 tokenId) external payable virtual override onlyOwnedByContract(tokenId) {
+        // TODO: check if token sales are enabled (targetSupply)
+        if (msg.value < reservePrice) revert InsufficientFunds(msg.value, reservePrice);
+
+        _tokenBalances[tokenId] = msg.value - reservePrice;
+        _lastSettlements[tokenId] = block.timestamp;
+
+        emit Purchase(tokenId, reservePrice);
+        _transfer(address(this), _msgSender(), tokenId);
+    }
+
+    function relinquish(uint256 tokenId) external virtual override onlyAuthorized(tokenId) onlySolvent(tokenId) {
+        _settle(tokenId);
+        _relinquish(tokenId);
+    }
+
+    function setPrice(
+        uint256 tokenId,
+        uint256 newPrice
+    ) external virtual override onlyAuthorized(tokenId) onlySolvent(tokenId) {
+        _settle(tokenId);
+        _prices[tokenId] = newPrice;
+
+        emit PriceChange(tokenId, newPrice);
+    }
+
+    function getPrice(uint256 tokenId) external view virtual override returns (uint256) {
+        return _prices[tokenId];
+    }
+
+    function withdrawAccountBalance(address account, uint256 amount) external virtual override {
+        // TODO: only withdraw own balance or can anyone trigger this?
+        uint256 balance = _accountBalances[account];
+        if (amount > balance) revert InsufficientFunds(balance, amount);
+
+        _accountBalances[account] -= amount;
+        _pay(account, amount);
+    }
+
+    function getAccountBalance(address account) external view virtual override returns (uint256) {
+        return _accountBalances[account];
+    }
+
+    function depositTokenBalance(uint256 tokenId) external payable virtual onlySolvent(tokenId) {
+        // TODO: check if token is owned by contract (?)
+
+        _tokenBalances[tokenId] += msg.value;
+
+        emit Deposit(tokenId, msg.value);
+    }
+
+    function withdrawTokenBalance(
+        uint256 tokenId,
+        uint256 amount
+    ) external virtual override onlyAuthorized(tokenId) onlySolvent(tokenId) {
+        _settle(tokenId);
+
+        uint256 balance = _tokenBalances[tokenId];
+        if (amount > balance) revert InsufficientFunds(balance, amount);
+
+        _tokenBalances[tokenId] -= amount;
+
+        emit Withdrawal(tokenId, amount);
+    }
+
+    function getTokenBalance(uint256 tokenId) external view virtual override returns (uint256) {
+        return _tokenBalances[tokenId];
+    }
+
+    function settleFees(uint256 tokenId) external virtual override notOwnedByContract(tokenId) {
+        bool insolvent = _isInsolvent(tokenId);
+
+        _settle(tokenId);
+        if (insolvent) _foreclose(tokenId);
+    }
+
+    function pendingFees(uint256 tokenId) external view virtual override returns (uint256) {
+        return _pendingFees(tokenId);
+    }
+
+    function isInsolvent(uint256 tokenId) external view virtual override returns (bool) {
+        return _isInsolvent(tokenId);
+    }
+
+    function insolvencyTime(uint256 tokenId) external view virtual override returns (uint256) {
+        uint256 feesPerSecond = _calculateFee(_prices[tokenId], 1);
+        if (feesPerSecond == 0) return _lastSettlements[tokenId];
+
+        return _lastSettlements[tokenId] + _tokenBalances[tokenId] / feesPerSecond;
+    }
+
+    function placeBid(uint256 maxPrice, uint256 newPrice) external payable virtual override returns (uint256) {
+        if (msg.value < maxPrice) revert InsufficientFunds(msg.value, maxPrice);
+
+        uint256 id = _nonce++;
+        address bidder = _msgSender();
+
+        _bids[id] = Bid({
+            timestamp: uint96(block.timestamp),
+            bidder: bidder,
+            value: msg.value,
+            maxPrice: maxPrice,
+            newPrice: newPrice
+        });
+
+        emit BidPlacement(id, bidder, maxPrice, newPrice);
+        return id;
+    }
+
+    function getBid(uint256 id) external view virtual override returns (Bid memory) {
+        return _bids[id];
+    }
+
+    function executeBid(uint256 id, uint256 tokenId) external virtual override onlySolvent(tokenId) {
+        Bid memory bid = _bids[id];
+        address owner = ownerOf(tokenId);
+
+        // TODO: restrict to bidder or admin (?)
+        bool authorized = _msgSender() == owner ||
+            (bid.maxPrice >= _prices[tokenId] && bid.timestamp + gracePeriod > block.timestamp);
+        if (!authorized) revert Unauthorized();
+
+        _settle(tokenId);
+
+        _accountBalances[owner] += bid.maxPrice;
+        _accountBalances[owner] += _tokenBalances[tokenId];
+
+        uint256 deposit = bid.value - bid.maxPrice;
+        _tokenBalances[tokenId] = deposit;
+        _prices[tokenId] = bid.newPrice;
+
+        delete _bids[id];
+
+        emit BidExecution(id, tokenId, bid.bidder, bid.maxPrice, bid.newPrice);
+        _transfer(owner, bid.bidder, tokenId);
+    }
+
+    function cancelBid(uint256 id) external virtual override {
+        Bid memory bid = _bids[id];
+        if (_msgSender() != bid.bidder) revert Unauthorized();
+
+        delete _bids[id];
+
+        emit BidCancellation(id, bid.bidder, bid.maxPrice, bid.newPrice);
+        _pay(bid.bidder, bid.value);
+    }
+
+    function _isInsolvent(uint256 tokenId) internal view virtual returns (bool) {
+        return ownerOf(tokenId) == address(this) || _pendingFees(tokenId) > _tokenBalances[tokenId];
+    }
+
+    function _checkSolvent(uint256 tokenId) internal view virtual {
+        if (_isInsolvent(tokenId)) revert InsolventToken(tokenId);
+    }
+
+    function _pendingFees(uint256 tokenId) internal view virtual returns (uint256) {
+        if (ownerOf(tokenId) == address(this)) return 0;
+
+        return _calculateFee(_prices[tokenId], block.timestamp - _lastSettlements[tokenId]);
+    }
+
+    function _calculateFee(uint256 price, uint256 duration) internal view virtual returns (uint256) {
+        return (price * duration * feeRate) / (feePeriod * feeDenominator);
+    }
+
+    function _settle(uint256 tokenId) internal {
+        uint256 pending = _pendingFees(tokenId);
+        uint256 available = _tokenBalances[tokenId];
+
+        uint256 amount = pending < available ? pending : available;
+
+        _tokenBalances[tokenId] -= amount;
+        _lastSettlements[tokenId] = block.timestamp;
+
+        emit Settlement(tokenId, amount);
+        _pay(beneficiary, amount);
+    }
+
+    function _foreclose(uint256 tokenId) internal {
+        address owner = ownerOf(tokenId);
+
+        _accountBalances[owner] += reservePrice;
+
+        _tokenBalances[tokenId] = 0;
+        _prices[tokenId] = 0;
+
+        emit Foreclosure(tokenId);
+        _transfer(owner, address(this), tokenId);
+    }
+
+    function _relinquish(uint256 tokenId) internal {
+        address owner = ownerOf(tokenId);
+
+        _accountBalances[owner] += reservePrice;
+        _accountBalances[owner] += _tokenBalances[tokenId];
+
+        _tokenBalances[tokenId] = 0;
+        _prices[tokenId] = 0;
+
+        emit Relinquishment(tokenId);
+        _transfer(owner, address(this), tokenId);
+    }
+
+    function _pay(address recipient, uint256 amount) internal {
+        if (amount > 0) Address.sendValue(payable(recipient), amount);
+    }
+}
---
+++ src/interfaces/IHarber.sol
@@ -0,0 +1,95 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.21;
+
+interface IHarber {
+    // STRUCTS
+    struct Bid {
+        uint96 timestamp;
+        address bidder;
+        uint256 value;
+        uint256 maxPrice;
+        uint256 newPrice;
+    }
+
+    // EVENTS
+    event BidPlacement(uint256 indexed id, address indexed bidder, uint256 maxPrice, uint256 newPrice);
+    event BidCancellation(uint256 indexed id, address indexed bidder, uint256 maxPrice, uint256 newPrice);
+    event BidExecution(
+        uint256 indexed id,
+        uint256 indexed tokenId,
+        address indexed bidder,
+        uint256 maxPrice,
+        uint256 newPrice
+    );
+
+    event Purchase(uint256 indexed tokenId, uint256 price);
+
+    event Settlement(uint256 indexed tokenId, uint256 amount);
+    event Foreclosure(uint256 indexed tokenId);
+    event Relinquishment(uint256 indexed tokenId);
+    event PriceChange(uint256 indexed tokenId, uint256 newPrice);
+
+    event Deposit(uint256 indexed tokenId, uint256 amount);
+    event Withdrawal(uint256 indexed tokenId, uint256 amount);
+
+    // ERRORS
+    error Unauthorized();
+    error InsolventToken(uint256 tokenId);
+    error InsufficientFunds(uint256 available, uint256 needed);
+    error IncorrectOwner();
+
+    // FUNCTIONS
+    function buy(uint256 tokenId) external payable;
+
+    function relinquish(uint256 tokenId) external;
+
+    function reservePrice() external view returns (uint256);
+
+    // Token price functions
+
+    function setPrice(uint256 tokenId, uint256 newPrice) external;
+
+    function getPrice(uint256 tokenId) external view returns (uint256);
+
+    // Account balance functions
+
+    function withdrawAccountBalance(address account, uint256 amount) external;
+
+    function getAccountBalance(address account) external view returns (uint256);
+
+    // Token balance functions
+
+    function depositTokenBalance(uint256 tokenId) external payable;
+
+    function withdrawTokenBalance(uint256 tokenId, uint256 amount) external;
+
+    function getTokenBalance(uint256 tokenId) external view returns (uint256);
+
+    // Fee functions
+
+    // TODO: do we need separate functions for tax settlement and foreclosure?
+    function settleFees(uint256 tokenId) external;
+
+    function feePeriod() external view returns (uint256);
+
+    function feeRate() external view returns (uint256);
+
+    function pendingFees(uint256 tokenId) external view returns (uint256);
+
+    function isInsolvent(uint256 tokenId) external view returns (bool);
+
+    function insolvencyTime(uint256 tokenId) external view returns (uint256);
+
+    // TODO: interface for handling user balances
+    // function getBalance(address account) external view returns (uint256);
+    // function withdraw(address account) external;
+
+    // TODO: single token bids vs collection bids
+    function placeBid(uint256 maxPrice, uint256 newPrice) external payable returns (uint256);
+
+    function getBid(uint256 id) external view returns (Bid memory);
+
+    function executeBid(uint256 id, uint256 tokenId) external;
+
+    function cancelBid(uint256 id) external;
+}
